7c194333a270be4c59e8e08858674134c7a91bd2
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-04-22 11:49:43 -0300

    creating model of Appointments using typeORM

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,18 +1,22 @@
-import { uuid } from 'uuidv4';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+	@PrimaryGeneratedColumn('uuid')
 	id: string;
 
+	@Column()
 	provider: string;
 
+	@Column('time with time zone')
 	date: Date;
 
-	// Omit <T, K -> A | B>
-	constructor({ provider, date }: Omit<Appointment, 'id'>) {
-		this.id = uuid();
-		this.provider = provider;
-		this.date = date;
-	}
+	// // Omit <T, K -> A | B>
+	// constructor({ provider, date }: Omit<Appointment, 'id'>) {
+	// 	this.id = uuid();
+	// 	this.provider = provider;
+	// 	this.date = date;
+	// }
 }
 
 export default Appointment;
